8ed5bcc17d30fa38626063b6dce9e32b0833aa2d
Willian Barroso <w.barroso@unesp.br>
2026-03-04 13:24:17 +0000

    Atualização para Deploy

---
+++ config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+    SUPABASE_URL: "https://tnerfwhghqldpwtqjtwj.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuZXJmd2hnaHFsZHB3dHFqdHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzY4NDUsImV4cCI6MjA4ODExMjg0NX0.oMz44nVcpWTJfubzeblStqWbYnoB7wDAeYNrJe257aA"
+};
